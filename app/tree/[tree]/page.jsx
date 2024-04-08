339caa3d120b387ce449d80fc2ad5f6ad6408bfb
sobhan_developer2002 <sobiesipiderman@gmail.com>
2024-04-08 14:20:03 +0330

    change tree page

---
+++ app/tree/[tree]/page.jsx
@@ -0,0 +1,23 @@
+import path from "path";
+import fs from "fs";
+
+import Tree from "@/components/tree/Tree";
+import { notFound } from "next/navigation";
+
+export default function TreePage({ params }) {
+  const dbPath = path.join(process.cwd(), "data", "names.json");
+  const data = fs.readFileSync(dbPath);
+  const parsedData = JSON.parse(data);
+
+  const treeData = parsedData.filter((item) => {
+    return item.id == params.tree;
+  });
+  if (treeData.length == 0) {
+    notFound();
+  }
+  return (
+    <div>
+      <Tree data={treeData[0]} />
+    </div>
+  );
+}
---
+++ app/tree/page.jsx
@@ -0,0 +1,9 @@
+import MainPage from "@/components/tree/Main";
+
+export default function MainTreePage() {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+}
